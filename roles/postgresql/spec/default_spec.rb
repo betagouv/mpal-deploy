ad78ed64992d533c44e611e27c3f6c289bf2c98e
Thomas Pepio <tpepio@octo.com>
2017-06-22 16:36:42 +0200

    Create a default database and user. Add tests to prevent regression.

--- roles/postgresql/spec/default_spec.rb
+++ roles/postgresql/spec/default_spec.rb
@@ -7,16 +7,30 @@ describe file('/etc/hosts') do
 end
 
 context 'PostgreSQL should run properly' do
-  describe file('/etc/postgresql/9.4/main/postgresql.conf') do
-    it { should be_file }
-    it { should be_owned_by 'postgres' }
-    it { should be_grouped_into 'postgres' }
-    it { should contain 'listen_addresses = *' }
-    it { should contain 'port = 5432' }
+  context 'As root' do
+    describe file('/etc/postgresql/9.4/main/postgresql.conf') do
+      it { should be_file }
+      it { should be_owned_by 'postgres' }
+      it { should be_grouped_into 'postgres' }
+      it { should contain 'listen_addresses = *' }
+      it { should contain 'port = 5432' }
+    end
+
+    describe service('postgresql') do
+      it { should be_running }
+      it { should be_enabled }
+    end
   end
 
-  describe service('postgresql') do
-    it { should be_running }
-    it { should be_enabled }
+  context 'As postgres user' do
+    let(:sudo_options) { '-u postgres' }
+
+    describe command('/usr/bin/psql -c "\l" | grep mpal') do
+      its(:stdout) { should contain 'mpal      | mpal     | UTF8' }
+    end
+
+    describe command('/usr/bin/psql -c "\du" | grep mpal') do
+      its(:stdout) { should contain 'mpal      | Superuser' }
+    end
   end
 end
