ceddc1a768cd8c8eff0cd432e56133d983b187ba
Thomas Pepio <tpepio@octo.com>
2017-06-29 16:47:31 +0200

    Roles to install ruby from sources, and instal the MPAL webapp

---
+++ roles/ruby/spec/default_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+context 'Ruby should be properly installed' do
+  packages = %w[build-essential make openssl]
+  packages.each do |package|
+    describe package(package.to_s) do
+      it { should be_installed }
+    end
+  end
+
+  describe command('ruby -v') do
+    its(:stdout) { should contain 'ruby 2.3.1' }
+  end
+
+  describe file('/tmp/ruby-2.3.1.tar.bz2') do
+    it { should_not be_file }
+  end
+
+  describe file('/tmp/ruby-2.3.1') do
+    it { should_not be_file }
+    it { should_not be_directory }
+  end
+end
