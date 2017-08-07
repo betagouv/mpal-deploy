ec3053123af160bff6dd29d4616143d2a0995fa4
Thomas Pepio <tpepio@octo.com>
2017-08-07 10:35:29 +0200

    Simplify ruby installation

--- roles/ruby/spec/default_spec.rb
+++ roles/ruby/spec/default_spec.rb
@@ -1,13 +1,6 @@
 require 'spec_helper'
 
 context 'Ruby should be properly installed' do
-  packages = %w[libssl-dev zlib1g-dev libreadline-dev libgdbm-dev libcurl4-openssl-dev libicu-dev libpq-dev ]
-  packages.each do |package|
-    describe package(package.to_s) do
-      it { should be_installed }
-    end
-  end
-
   describe command('ruby -v') do
     its(:stdout) { should contain 'ruby 2.3.1' }
   end
